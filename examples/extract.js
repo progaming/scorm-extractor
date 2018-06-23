b534d215d626196e2fdff18737645341eb4fa444
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2018-06-23 14:13:11 +0700

    remove unused file

--- examples/extract.js
+++ examples/extract.js
@@ -1,4 +1,4 @@
-const extract = require('../lib');
+const extract = require('../index');
 
 const main = async() => {
     const navTree = await extract(__dirname + '/golf.zip' , __dirname + '/../content');
